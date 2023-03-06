3597d72b07d4918678a464f6341510ee40ed43c5
anderecc <andersondbl06@gmail.com>
2023-03-06 08:26:12 -0300

    add responsive mobile

--- 4-sticky-navbar/index.js
+++ 4-sticky-navbar/index.js
@@ -2,7 +2,6 @@ let $bottomContainer = document.querySelector('.bottom-container');
 let $navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', (e) => {
-    console.log($bottomContainer.offsetTop, $navbar.offsetHeight);
     window.scrollY >= $bottomContainer.offsetTop - $navbar.offsetHeight - 50
         ? $navbar.classList.add('active')
         : $navbar.classList.remove('active');
